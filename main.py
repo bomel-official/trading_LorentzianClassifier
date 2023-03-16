0247337c82dfa5d2c94b7a42bb20bc9f4e3d8c5d
bomel-official <rusihh228@gmail.com>
2023-03-16 18:50:58 +0300

    Linear regression

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 # ===== Получение данных ===== START
 import requests
 import pandas as pd
-import numpy as np
-from sklearn.preprocessing import StandardScaler
-from scipy.optimize import curve_fit
-import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
 
 # задать параметры запроса
 url = 'https://api.bybit.com/v5/market/kline'
@@ -14,7 +13,7 @@ interval = '15'
 start_time = pd.Timestamp.now(tz='UTC') - pd.Timedelta(days=7)
 start_time = start_time.replace(hour=0, minute=0, second=0, microsecond=0)
 start_time_str = start_time.isoformat()
-limit = 1000
+limit = 200
 
 # отправить запрос и получить данные
 params = {'symbol': symbol, 'category': category, 'interval': interval, 'from': start_time_str, 'limit': limit}
@@ -25,64 +24,33 @@ datalist = data['list']
 
 # преобразовать данные в формат DataFrame
 df = pd.DataFrame(datalist, columns=['startTime', 'openPrice', 'highPrice', 'lowPrice', 'closePrice', 'volume', 'turnover'])
-df = df.astype(float)
 df['time'] = pd.to_datetime(df['startTime'], unit='ms')
 df.set_index('time', inplace=True)
-df.drop(['startTime', 'volume', 'turnover'], axis=1, inplace=True)
-df.columns = ['open', 'high', 'low', 'close']
+df.drop(['startTime', 'turnover'], axis=1, inplace=True)
+df.columns = ['open', 'high', 'low', 'close', 'volume']
+df = df.astype(float)
 
 # ===== Получение данных ===== END
 
+# Выберем признаки для модели
+X = df[['open', 'high', 'low', 'volume']]
+y = df['close']
 
-def lorentzian_func(x, A, gamma, x0):
-    return (A*gamma**2)/((x-x0)**2+gamma**2)
+# Разделим данные на обучающую и тестовую выборки
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-def fit_lorentzian(xdata, ydata):
-    popt, _ = curve_fit(lorentzian_func, xdata, ydata, p0=[max(ydata), 1, xdata.mean()])
-    return popt
+# Создадим модель линейной регрессии
+model = LinearRegression()
 
-class LorentzianClassifier:
-    def __init__(self, n_bins, threshold):
-        self.n_bins = n_bins
-        self.threshold = threshold
-        self.centers = []
-        self.widths = []
-        
-    def fit(self, data):
-        hist, bin_edges = np.histogram(data, bins=self.n_bins, density=True)
-        bin_centers = (bin_edges[:-1] + bin_edges[1:]) / 2
-        popt = fit_lorentzian(bin_centers, hist)
-        self.centers.append(popt[2])
-        self.widths.append(popt[1])
-        
-    def predict(self, data):
-        pred = []
-        for val in data:
-            dist = [np.abs(val-c) for c in self.centers]
-            closest_center = self.centers[np.argmin(dist)]
-            closest_width = self.widths[np.argmin(dist)]
-            if np.abs(val - closest_center) < closest_width * self.threshold:
-                pred.append(1)
-            else:
-                pred.append(0)
-        return np.array(pred)
+# Обучим модель на обучающей выборке
+model.fit(X_train, y_train)
 
-# Подготовка данных для анализа
-scaler = StandardScaler()
-scaled_data = scaler.fit_transform(df)
+# Сделаем предсказания на тестовой выборке
+y_pred = model.predict(X_test)
 
-# Создание и обучение модели Lorentzian Classification
-n_bins = 50
-threshold = 2
-lc = LorentzianClassifier(n_bins=n_bins, threshold=threshold)
-lc.fit(scaled_data)
+# Оценим качество модели
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
 
-# Получение предсказаний модели
-predictions = lc.predict(scaled_data)
-
-# Визуализация результатов
-import matplotlib.pyplot as plt
-
-plt.plot(df.index, df['close'].values)
-plt.plot(df.index, scaler.inverse_transform(predictions)[:,3], 'r')
-plt.show()
+print('Mean Squared Error:', mse)
+print('R^2 Score:', r2)
